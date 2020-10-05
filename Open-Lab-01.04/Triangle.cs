e1ac79e0e9d99360b37d6ce93a735a1f1fa6afb5
Misko183 <rtmike183@gmail.com>
2020-10-05 08:33:27 +0200

    implement a method on triangel

--- Open-Lab-01.04/Triangle.cs
+++ Open-Lab-01.04/Triangle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Open_Lab_01._04
 {
@@ -6,7 +7,8 @@ namespace Open_Lab_01._04
     {
         public double Area(double foundation, double height)
         {
-            throw new NotImplementedException();
+            double area = foundation * height / 2;
+            return area;
         }
     }
 }
